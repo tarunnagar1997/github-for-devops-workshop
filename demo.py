e9982b7240ba49085302ce549ce8e8e592ff9508
Tarun Nagar <yesiamtarun01@gmail.com>
2025-09-27 16:32:04 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 def my_function():
     """
-    This is a demo function that initializes a variable and returns it."""
+    This is a demo function that initializes a variable and returns it.
+    """
+    
     a = 3
     return a
 
